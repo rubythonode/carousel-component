e5276d9def48c66d1d7c7757adcdbfda461908dc
york yao <yaoao12306@outlook.com>
2017-05-29 11:50:19 +0800

    add animations

--- src/common.ts
+++ src/common.ts
@@ -3,3 +3,35 @@ export interface CarouselData {
     component?: string | Function;
     /* tslint:enable:ban-types */
 }
+
+const animationKeyframesIdName = "carousel-animation-keyframes";
+
+export function setStyle(num: number, width: number) {
+    let style = document.getElementById(animationKeyframesIdName) as HTMLStyleElement;
+    if (!style) {
+        style = document.createElement("style");
+        style.type = "text/css";
+        style.id = animationKeyframesIdName;
+        document.getElementsByTagName("head")[0].appendChild(style);
+    }
+    style.innerHTML = `@keyframes move-left {
+            0% { left: 0; }
+            100% { left: ${width * num}px; }
+        }
+        @keyframes move-right {
+            0% { left: 0; }
+            100% { left: -${width * num}px; }
+        }`;
+}
+
+export function runAnimation(ul: HTMLElement, timeout: number, keyframes: string, num: number, next: () => void) {
+    ul.style.animation = "";
+    window.requestAnimationFrame(() => {
+        window.requestAnimationFrame(() => {
+            ul.style.animation = `${timeout / 1000}s linear 0s normal ${keyframes}`;
+            setTimeout(() => {
+                next();
+            }, timeout);
+        });
+    });
+}
--- src/vue.ts
+++ src/vue.ts
@@ -16,17 +16,71 @@ class Carousel extends Vue {
     timer: NodeJS.Timer;
     currentIndex = 0;
     width = 200;
+    hoveringLeft = false;
+    hoveringRight = false;
+    currentCount = 0;
 
     beforeMount() {
-        this.timer = setInterval(() => {
-            this.currentIndex++;
-        }, this.interval);
+        this.currentCount = +this.count;
+        this.data.push(...this.data.slice(0, this.currentCount));
+        this.start();
     }
 
-    get ulStyle() {
+    get leftStyle() {
         return {
-            left: -this.currentIndex * this.width + "px",
+            opacity: this.hoveringLeft ? 100 : 0,
         };
+    }
+
+    get rightStyle() {
+        return {
+            opacity: this.hoveringRight ? 100 : 0,
+        };
+    }
+
+    moveLeft(num: number) {
+        common.setStyle(num, 200);
+        common.runAnimation(this.$refs.ul as HTMLElement, this.timeout, "move-left", num, () => {
+            this.currentIndex -= num;
+            if (this.currentIndex < 0) {
+                this.currentIndex = this.data.length - this.currentCount + this.currentIndex;
+            }
+        });
+    }
+    moveRight(num: number) {
+        common.setStyle(num, 200);
+        common.runAnimation(this.$refs.ul as HTMLElement, this.timeout, "move-right", num, () => {
+            this.currentIndex += num;
+            if (this.currentIndex >= this.data.length - this.currentCount) {
+                this.currentIndex -= (this.data.length - this.currentCount);
+            }
+        });
+    }
+    pause() {
+        if (this.timer) {
+            clearInterval(this.timer);
+        }
+    }
+    start() {
+        this.timer = setInterval(() => {
+            this.moveRight(1);
+        }, this.interval);
+    }
+    mouseenterLeft() {
+        this.hoveringLeft = true;
+        this.pause();
+    }
+    mouseleaveLeft() {
+        this.hoveringLeft = false;
+        this.start();
+    }
+    mouseenterRight() {
+        this.hoveringRight = true;
+        this.pause();
+    }
+    mouseleaveRight() {
+        this.hoveringRight = false;
+        this.start();
     }
 }
 
